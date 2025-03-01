db14af14a77b286e42ce597fb2469938fde533d1
kokoShine <kokoshine@gmail.com>
2025-03-01 14:28:56 +0800

    create vpc2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "hello_vpc-1" {
+#resource "aws_vpc" "hello_vpc-1" {
 
-  cidr_block           = "192.166.1.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-}
+#  cidr_block           = "192.166.1.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#
+#}
