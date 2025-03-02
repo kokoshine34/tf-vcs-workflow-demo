2f3244989b0c7cce85d145247097a4bbccc211ec
kokoShine <kokoshine@gmail.com>
2025-03-03 03:06:50 +0800

    newvpc

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  cloud {
+    organization = "koko-master-programmatic-admin"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+    hostname = "app.terraform.io"
+
+    workspaces {
+      name = "tf-vcs-workflow-demo"
+      # name = "tf-vcs-workflow-planonly"
+    }
+  }
+}
