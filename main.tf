7415fb4e177c9f257e4e398a0b8862ad29c58063
kokoShine <kokoshine@gmail.com>
2025-03-03 02:40:24 +0800

    newvpc

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "aws_vpc" "hello_vpc" {
+#resource "aws_vpc" "hello_vpc" {
 
-  cidr_block           = "192.168.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#  cidr_block           = "192.168.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
 
-}
+#}
 
-resource "aws_vpc" "hello_vpc-1" {
+#resource "aws_vpc" "hello_vpc-1" {
 
-  cidr_block           = "192.2.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+#  cidr_block           = "192.2.0.0/16"
+#  enable_dns_hostnames = true
+ # enable_dns_support   = true
 
-}
+#}
