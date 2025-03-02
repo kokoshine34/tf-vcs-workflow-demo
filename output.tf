956bee5d8736c373efbda3f4df89b1a57c0cd969
kokoShine <kokoshine@gmail.com>
2025-03-03 02:50:38 +0800

    newvpc

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-#output "vpc_id" {
-#  value       = aws_vpc.hello_vpc.id
-#  description = "VPC ID."
-#}
+output "vpc_id" {
+  value       = aws_vpc.hello_vpc.id
+  description = "VPC ID."
+}
 
-#output "vpc_id-2" {
-#  value       = aws_vpc.hello_vpc.id
-#  description = "VPC ID2."
-#}
+output "vpc_id-2" {
+  value       = aws_vpc.hello_vpc.id
+  description = "VPC ID2."
+}
